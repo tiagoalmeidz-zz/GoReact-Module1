85dbd9fc3e107e5c060ea30badc702ce306d67cb
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-05-05 12:03:56 -0300

    Componentes com React

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-const soma = (a, b) => a + b;
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-alert(soma(5, 8));
+class Button extends Component {
+  render() {
+    return <a href="">Enviar</a>;
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Hello Rocketseat!</h1>
+        <Button />
+      </>
+    );
+  }
+}
+
+render(<App />, document.getElementById("app"));
